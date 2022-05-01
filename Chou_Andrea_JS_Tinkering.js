d9530c4d035b5a6a24f2c613e69898f8d9d90f80
Andrea Chou <35553710+andreachou@users.noreply.github.com>
2022-05-01 11:17:32 -0700

    Update Chou_Andrea_JS_Tinkering.js

--- Chou_Andrea_JS_Tinkering.js
+++ Chou_Andrea_JS_Tinkering.js
@@ -1,4 +1,6 @@
 /*
+JS function and for loop practice:
+
 The Story: We want an app that once it measures a kid's height, can display whether the child is tall enough to ride the rollercoaster. We're tasked with building the function that will display the right message to the child.
 
 Step 1: Create the following variable:
